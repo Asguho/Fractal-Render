c65c3577f983e5624c711da7ea13b8011aade358
Asguho <78410939+Asguho@users.noreply.github.com>
2024-05-14 13:43:34 +0200

    not working

--- demo/src/main/java/com/example/FractalCanvas.java
+++ demo/src/main/java/com/example/FractalCanvas.java
@@ -3,6 +3,7 @@ package com.example;
 import processing.core.PApplet;
 import processing.core.PConstants;
 import processing.core.PImage;
+import processing.core.PVector;
 
 public class FractalCanvas {
     private int width, height, maxIterations = 50, resolutionFactor = 2;
@@ -10,6 +11,7 @@ public class FractalCanvas {
     private PApplet p;
     String lastImageInputHash = "";
     PImage img;
+    int chunkSize = 5;
 
     public FractalCanvas(PApplet p, int width, int height, float magnificationFactor, float moveX, float moveY) {
         this.p = p;
@@ -28,14 +30,144 @@ public class FractalCanvas {
         this.resolutionFactor = PApplet.max(resolutionFactor, 1);
     }
 
+    public void setChunkSize(int chunkSize) {
+        this.chunkSize = PApplet.max(chunkSize, 1);
+    }
+
     public void draw() {
         if (!lastImageInputHash.equals(getInputHash())) {
+            float dim = calcFrataIDimension();
+            System.err.println("dim: " + dim);
             // && (p.frameRate > 30)
             // img = mandelbrot();
-            img = getChunksWithEdge(sobelEdgeDetection(blur(mandelbrot())), 5);
+            // img = getChunksWithEdge(sobelEdgeDetection(blur(mandelbrot())), 5);
+            // img = mandelbrot();
+            // loop though chunks with edge
+
+            // img = julia();
             lastImageInputHash = getInputHash();
+            // // test linear regression
+            // float[] x = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f };
+            // float[] y = { 2.0f, 4.0f, 6.0f, 8.0f, 10.0f };
+            // float[] result1 = linearRegression(x, y);
+            // float[] result = calculateFractalDimension(img);
+            // System.err.println("result1: " + result1[0] + ", " + result1[1]);
+            // System.err.println("result: " + result[0] + ", " + result[1]);
+
         }
-        p.image(img, 400, 0);
+        // p.image(img, 400, 0);
+        // drawChunks(img);
+
+    }
+
+    private void drawChunks(PImage inputImage) {
+        PVector[] chunks = getChunksWithEdge(sobelEdgeDetection(img), chunkSize);
+        p.noFill();
+        p.strokeWeight(1);
+        p.stroke(255, 0, 0);
+        for (int i = 0; i < chunks.length; i++) {
+            p.rect(chunks[i].x + 400, chunks[i].y, chunkSize, chunkSize);
+        }
+
+    }
+
+    private float[] calculateFractalDimension(PImage inputImage) {
+        float[] chunks = new float[10];
+        float[] chunksSize = new float[10];
+        for (int i = 1; i < 10; i++) {
+            chunksSize[i] = i * 2;
+            chunks[i] = getAmountOfChunksWithEdge(sobelEdgeDetection(img), i * 2);
+        }
+        float[] x = new float[10];
+        float[] y = new float[10];
+        for (int i = 1; i < 10; i++) {
+            x[i] = PApplet.log(1 / (float) chunksSize[i]);
+            y[i] = PApplet.log((float) chunks[i]);
+            System.err.println(
+                    "x: " + x[i] + ", y: " + y[i] + ", chunks: " + chunks[i] + ", chunksSize: " + chunksSize[i]
+                            + " calc: "
+                            + PApplet.log((float) chunks[i]) + ", " + (1 / (float) chunksSize[i]));
+        }
+        return linearRegression(x, y);
+    }
+
+    // public float calcFrataIDimension()
+    // "Fratat": Unknown word.
+    // long[] totatPixets
+    // = new tong [18];
+    // for (int scalingFactor = 18; scalingFactor 1; scalingFactor—) {
+    // // 1600, 1000, scalingFactor, 100, 1.4, -1.2d, O.Od, 9d, Od
+    // FractalParams params = new z:1.4d, -1.2d, oy:O.Od,
+    // w:40G9,
+    // h :4000,
+    // scatingFactor) ;
+    // Plmage img = drawmandetbrot(params) ;
+    // img = twoToneFiIter (img) ;
+    // Img = sobeIEdgeDetection (img) ;
+    // + scalingFactor +
+    // tong whitePixets = O
+    // img. toadPixeIs ( ) ;
+    // for (int x = O; x <
+    // img.width; x++) {
+    // png") ;
+    // for (int y = 8; y < img.height; Y++) {
+    // int c = img.pixets[x + y * img.width] ;
+    // = 255 p.blue(c) = 255) {
+    // if (p. red(c)
+    // = 255 && p. green(c)
+    // whitePixe1s ;
+    // totatPixets [ scalingFactor
+    // pixels:
+    // float[] x = new
+    // float[] y = new float[10];
+    // for (int i = O; i < 18; i++) {
+    // whitePixets ;
+    // + whitePixets) ;
+    // x[i] = PApptet.tog(1.Of / (float) (i + 1));
+    // = totatPixets[i]);
+    // float[] regression = linearRegression(x, y) ;
+    // return regression [1];
+
+    private float calcFrataIDimension() {
+        long[] totalPixels = new long[10];
+        for (int scalingFactor = 10; scalingFactor >= 1; scalingFactor--) {
+            PImage img;
+            img = sobelEdgeDetection(mandelbrot(scalingFactor));
+            int whitePixels = 0;
+            img.loadPixels();
+            for (int x = 0; x < img.width; x++) {
+                for (int y = 0; y < img.height; y++) {
+                    int c = img.pixels[x + y * img.width];
+                    if (p.red(c) == 255 && p.green(c) == 255 && p.blue(c) == 255) {
+                        whitePixels++;
+                    }
+                }
+            }
+            totalPixels[scalingFactor - 1] = whitePixels;
+            System.err.println("whitePixels: " + whitePixels);
+        }
+        float[] x = new float[10];
+        float[] y = new float[10];
+        for (int i = 0; i < 10; i++) {
+            x[i] = PApplet.log(1.0f / (float) (i + 1));
+            y[i] = PApplet.log((float) totalPixels[i]);
+            System.err.println("x: " + x[i] + ", y: " + y[i] + ", totalPixels: " + totalPixels[i]);
+        }
+        float[] regression = linearRegression(x, y);
+        return regression[0];
+    }
+
+    private float[] reverseFloatArray(float[] array) {
+        float[] reversedArray = new float[array.length];
+        for (int i = 0; i < array.length; i++) {
+            reversedArray[array.length - 1 - i] = array[i];
+        }
+        return reversedArray;
+    }
+
+    private int getAmountOfChunksWithEdge(PImage inputImage, int chunkSize) {
+        PVector[] chunks = getChunksWithEdge(sobelEdgeDetection(img), chunkSize);
+        return chunks.length;
     }
 
     public void keyPressed(char key) {
@@ -61,22 +193,44 @@ public class FractalCanvas {
 
     private String getInputHash() {
         return width + " " + height + " " + magnificationFactor + " " + moveX + " " + moveY + " " + maxIterations + " "
-                + resolutionFactor;
+                + resolutionFactor + " " + chunkSize;
     }
 
-    private PImage mandelbrot() {
+    private PImage mandelbrot(int resolutionFactor) {
         PImage img = p.createImage(width / resolutionFactor, height / resolutionFactor, PConstants.RGB);
         for (int x = 0; x < img.width; x++) {
             for (int y = 0; y < img.height; y++) {
-                double zx = 0;
-                double zy = 0;
-                double cX = (x - img.width / 2) / (magnificationFactor * img.width / 4) - moveX;
-                double cY = (y - img.height / 2) / (magnificationFactor * img.height / 4) - moveY;
+                double zR = 0;
+                double zI = 0;
+                double cR = (x - img.width / 2) / (magnificationFactor * img.width / 4) - moveX;
+                double cI = (y - img.height / 2) / (magnificationFactor * img.height / 4) - moveY;
                 int iter = maxIterations;
-                while (zx * zx + zy * zy < 4 && iter > 0) {
-                    double tmp = zx * zx - zy * zy + cX;
-                    zy = 2.0f * zx * zy + cY;
-                    zx = tmp;
+                while (zR * zR + zI * zI < 4 && iter > 0) {
+                    double tmp = zR * zR - zI * zI + cR;
+                    zI = 2.0f * zR * zI + cI;
+                    zR = tmp;
+                    iter--;
+                }
+                img.pixels[x + y * img.width] = p.color(PApplet.map(iter, 0, maxIterations, 0, 255));
+            }
+        }
+        img.resize(width, height);
+        return img;
+    }
+
+    private PImage julia() {
+        PImage img = p.createImage(width / resolutionFactor, height / resolutionFactor, PConstants.RGB);
+        double cX = -0.7;
+        double cY = 0.27015;
+        for (int x = 0; x < img.width; x++) {
+            for (int y = 0; y < img.height; y++) {
+                double zR = (x - img.width / 2) / (magnificationFactor * img.width / 4) - moveX;
+                double zI = (y - img.height / 2) / (magnificationFactor * img.height / 4) - moveY;
+                int iter = maxIterations;
+                while (zR * zR + zI * zI < 4 && iter > 0) {
+                    double tmp = zR * zR - zI * zI + cX;
+                    zI = 2.0f * zR * zI + cY;
+                    zR = tmp;
                     iter--;
                 }
                 img.pixels[x + y * img.width] = p.color(PApplet.map(iter, 0, maxIterations, 0, 255));
@@ -130,8 +284,8 @@ public class FractalCanvas {
         return outputImage;
     }
 
-    private PImage getChunksWithEdge(PImage inputImage, int chunkSize) {
-        PImage outputImage = p.createImage(inputImage.width, inputImage.height, PConstants.RGB);
+    private PVector[] getChunksWithEdge(PImage inputImage, int chunkSize) {
+        PVector[] chunks = new PVector[0];
         int numberOfChunks = 0;
         for (int x = 0; x < inputImage.width; x += chunkSize) {
             for (int y = 0; y < inputImage.height; y += chunkSize) {
@@ -146,22 +300,36 @@ public class FractalCanvas {
                 }
                 int avg = sum / (chunkSize * chunkSize);
 
-                if (avg > 0) {
-                    // System.err.println("avg > 0: " + avg);
+                if (avg > 255 / 4) {
                     numberOfChunks++;
-
-                    for (int i = 0; i < chunkSize; i++) {
-                        for (int j = 0; j < chunkSize; j++) {
-                            int loc = (x + i) + (y + j) * inputImage.width;
-                            if (loc >= 0 && loc < inputImage.pixels.length) {
-                                outputImage.pixels[loc] = p.color(255);
-                            }
-                        }
+                    PVector[] newChunks = new PVector[chunks.length + 1];
+                    for (int i = 0; i < chunks.length; i++) {
+                        newChunks[i] = chunks[i];
                     }
+                    newChunks[chunks.length] = new PVector(x, y);
+                    chunks = newChunks;
                 }
             }
         }
-        System.err.println("numberOfChunks: " + numberOfChunks + ", chunksize: " + chunkSize);
-        return outputImage;
+        System.err.println("numberOfChunks: " + numberOfChunks + ", chunkSize: " + chunkSize);
+
+        return chunks;
     }
+
+    private float[] linearRegression(float[] x, float[] y) {
+        float sumX = 0;
+        float sumY = 0;
+        float sumXY = 0;
+        float sumX2 = 0;
+        for (int i = 0; i < x.length; i++) {
+            sumX += x[i];
+            sumY += y[i];
+            sumXY += x[i] * y[i];
+            sumX2 += x[i] * x[i];
+        }
+        float m = (x.length * sumXY - sumX * sumY) / (x.length * sumX2 - sumX * sumX);
+        float b = (sumY - m * sumX) / x.length;
+        return new float[] { m, b };
+    }
+
 }
--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -6,6 +6,9 @@ public class Main extends processing.core.PApplet {
     FractalCanvas fractalCanvas = new FractalCanvas(this, 800, 800, 1.0f, 0.0f, 0.0f);
     IntInputField maxIterationsInputField = new IntInputField(this, "Max Iterations", 10, 60, "100", true);
     IntInputField resolutionFactorInputField = new IntInputField(this, "resolutionFactor", 10, 90, "1", true);
+    IntInputField xInputField = new IntInputField(this, "x", 10, 120, "0", true);
+    IntInputField yInputField = new IntInputField(this, "y", 10, 150, "0", true);
+    IntInputField chunkSizeInputField = new IntInputField(this, "chunkSize", 10, 180, "5", true);
 
     public static void main(String[] args) {
         PApplet.main("com.example.Main", args);
@@ -26,9 +29,15 @@ public class Main extends processing.core.PApplet {
         text("FPS: " + frameRate, 10, 20);
         maxIterationsInputField.draw();
         resolutionFactorInputField.draw();
+        xInputField.draw();
+        yInputField.draw();
+        chunkSizeInputField.draw();
 
         fractalCanvas.setMaxIterations(maxIterationsInputField.getValue());
         fractalCanvas.setResolutionFactor(resolutionFactorInputField.getValue());
+        // fractalCanvas.moveX = xInputField.getValue();
+        // fractalCanvas.moveY = yInputField.getValue();
+        fractalCanvas.setChunkSize(chunkSizeInputField.getValue());
     }
 
     public void keyPressed() {
